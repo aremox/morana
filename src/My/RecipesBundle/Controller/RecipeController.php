438ee0bea02c285828f98639a4cb475f35fb3b1f
aremox <arenasmorante@gmail.com>
2016-03-30 12:16:18 +0200

    ejercicio tema 2
    ejercicio tema 2

---
+++ src/My/RecipesBundle/Controller/RecipeController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace My\RecipesBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+class RecipeController extends Controller
+{
+    public function listAction($page)
+    {
+        return new Response('<html><body><p>No hay recetas</p></body></html>');
+    }
+
+    public function showAction($recipe_name, Request $request)
+    {
+        /*return $this->render('MyRecipesBundle:Recipe:show.html.twig',array(
+        		'recipe'=> $recipe_name,
+        		));
+        		*/
+        $session = $request->getSession();
+        $session->set('clave','valor');
+        $session->get('clave');
+        $session->getFlashBag()->add(
+        	'notice',
+        	'Has publicado una nueva receta');
+        //return new Response('<html><body><p>...</p></body></html>');
+		return $this->render('MyRecipesBundle:Recipe:show.html.twig', array(
+			'recipe' => $recipe));  
+    }
+}
